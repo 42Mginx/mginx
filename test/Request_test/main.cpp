00708de997243881b207467d088cbb8a88ab4a3f
Hyun Jun Eom <zzp1318@gmail.com>
2022-03-05 21:02:40 +0900

    Request err

--- test/Request_test/main.cpp
+++ test/Request_test/main.cpp
@@ -6,13 +6,18 @@ int main(void)
 {
 	std::string request_value;
 
-	request_value = "POST / HTTP/1.1 \
-					Host: localhost:8000 \
-					User-Agent: Go-http-client/1.1\
-					Transfer-Encoding: chunked\
-					Content-Type: test/file\
-					Accept-Encoding: gzip"\;
+	request_value = "POST /directory HTTP/1.1\r\n\
+Host: localhost:8000 \r\n\
+User-Agent: Go-http-client/1.1\r\n\
+Transfer-Encoding: chunked\r\n\
+Content-Type: test/file\r\n\
+Accept-Encoding: gzip\r\n";
 	
 	Request test(request_value);
 
+	std::cout << test.getMethod() << std::endl;
+	std::cout << test.getTargetPath() << std::endl;
+	std::cout << test.getVersion() << std::endl;
+
+	return (0);
 }
