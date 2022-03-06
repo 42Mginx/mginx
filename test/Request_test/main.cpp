d872871c08138505c43df88ec89b7db9c6a4f1ed
Hyun Jun Eom <zzp1318@gmail.com>
2022-03-06 11:45:00 +0900

    request end

--- test/Request_test/main.cpp
+++ test/Request_test/main.cpp
@@ -6,18 +6,17 @@ int main(void)
 {
 	std::string request_value;
 
-	request_value = "POST /directory HTTP/1.1\r\n\
+	request_value = "POST /directory?id=3&hi=2 HTTP/1.1 \r\n\
 Host: localhost:8000 \r\n\
 User-Agent: Go-http-client/1.1\r\n\
 Transfer-Encoding: chunked\r\n\
 Content-Type: test/file\r\n\
-Accept-Encoding: gzip\r\n";
+Accept-Encoding: gzip\r\n\
+\r\n\
+ddd\r\n\
+ssdsdsd\r\n\
+sdsdsdsd";
 	
 	Request test(request_value);
-
-	std::cout << test.getMethod() << std::endl;
-	std::cout << test.getTargetPath() << std::endl;
-	std::cout << test.getVersion() << std::endl;
-
 	return (0);
 }
